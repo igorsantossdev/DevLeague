fd6087313de8d1a11f6ae65ea8832cacf9c0d5cc
Igor Santos <127339285+igorsantossdev@users.noreply.github.com>
2024-09-04 07:52:49 -0300

    Delete Dev League  directory

--- Dev League /js/app.js
+++
@@ -1,6 +0,0 @@
-// Exemplo de manipulação com JS se necessário
-document.querySelectorAll('.cell').forEach(cell => {
-    cell.addEventListener('click', function() {
-        alert('Você clicou em uma célula!');
-    });
-});
